8f8a46fc5fb97dffaa955746e77513bcffb905a8
Heather Libecki <tigger2013@mail.fresnostate.edu>
2018-11-27 16:37:36 -0800

    added the check and delete buttons on the add pins page

--- src/components/addpinmap/addpinmap.component.js
+++ src/components/addpinmap/addpinmap.component.js
@@ -1,6 +1,6 @@
 import styles from './addpinmap.component.style.js';
 import myMapStyle from '../map/mapstyle';
-import { Container, Header, Content, Button, Text, Icon as NativeIcon, Footer} from 'native-base';
+import { Container, Header, Content, Button, Text, Icon, Fab, Footer} from 'native-base';
 import Expo, { Constants, Location, Permissions } from 'expo';
 import MapView, { Marker } from 'react-native-maps';
 import API, { graphqlOperation } from '@aws-amplify/api'
@@ -72,20 +72,25 @@ export default class AddPinMap extends Component {
             <Image source={redPin} style={{transform: [{ scale: .35 }]}}/>
           </View>
 
-          <View style={styles.button1Container}>
-            <Button large rounded danger
-            onPress={() => this.props.navigation.navigate('Map')}
-            >
-              <NativeIcon type="FontAwesome" name="chevron-circle-left" />
-            </Button>
+            <View style={styles.button1Container}>
+            <Fab
+            style={{ backgroundColor: '#ed2224' }}
+            position="bottomLeft"
+            onPress={() => this.props.navigation.navigate('Map')}>
+            <Icon name="close" />
+            </Fab>
+            
           </View>
           <View style={styles.button2Container}>
-            <Button large rounded success
-              onPress={() => this.props.navigation.navigate('PinInfo')}
+            <Fab
+            style={{ backgroundColor: '#79e56a' }}
+            position="bottomRight"
+            onPress={() => this.props.navigation.navigate('PinInfo')}
             >
-              <NativeIcon type="FontAwesome" name="check-circle" />
-            </Button>
-          </View>
+            <Icon name="checkmark" />
+            </Fab>
+
+          </View> 
 
 
       </View>
